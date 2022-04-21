0cfdffc4981cac29aed5df351d609463d0d434ba
Audry Wolters <audrywolters@gmail.com>
2022-04-21 16:20:19 -0500

    created AddRating in JsonService

--- ContosoCrafts/Services/JsonFileProductService.cs
+++ ContosoCrafts/Services/JsonFileProductService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using ContosoCrafts.Models;
 using Microsoft.AspNetCore.Hosting;
@@ -30,6 +31,43 @@ namespace ContosoCrafts.Services
                         PropertyNameCaseInsensitive = true
                     });
             }
+        }
+
+        public void AddRating(string productId, int ratingArray)
+        {
+            var products = GetProducts();
+
+            // i want to name query -> product and type it to Product
+            // get the product in question
+            var query = products.First(x => x.Id == productId);
+
+            // check if it has ratings
+            if (query.Ratings == null)
+            {
+                query.Ratings = new int[] { ratingArray };
+            }
+            else
+            {
+                // if so, add it to Product object
+                var ratingList = query.Ratings.ToList();
+                ratingList.Add(ratingArray);
+                // then get it ready to put in json
+                query.Ratings = ratingList.ToArray();
+            }
+
+            // make it json
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+
         }
     }
 }
